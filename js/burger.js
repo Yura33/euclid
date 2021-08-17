72661ea069a959799b26e7a3a4facdff743b3b25
Yura33 <52007508+Yura33@users.noreply.github.com>
2021-08-17 22:38:13 +0200

    fix: adjust burger menu

--- js/burger.js
+++ js/burger.js
@@ -2,25 +2,32 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const btnBurger = document.querySelector('.header__burger');
   const headerNav = document.querySelector('.header__nav');
-  const headerLinks = document.querySelectorAll('.header__link');
-  const hero = document.querySelector('.hero__banner');
+  const logoLink = document.querySelector('.header__logo-link');
+  const hero = document.querySelector('.hero');
   const header = document.querySelector('.header');
+  const lastHeaderLink = document.querySelector('.header__item:last-child > .header__link');
 
   btnBurger.addEventListener('click', () => {
     btnBurger.classList.toggle('header__burger_active');
     headerNav.classList.toggle('header__nav_active');
 
     if(headerNav.classList.contains('header__nav_active')) {
-      headerNav.style.height = `${hero.scrollHeight + header.clientHeight}px`;
 
-    //   headerLinks.forEach((e) => {
-    //     e.setAttribute('tabindex', 0);
-    //   });
-    // } else {
-    //   headerLinks.forEach((e) => {
-    //     e.setAttribute('tabindex', -1);
-    //   });
+      headerNav.style.height = `${hero.scrollHeight + header.clientHeight}px`;
+      logoLink.tabIndex = -1;
+
+      lastHeaderLink.addEventListener('blur', () => {
+        logoLink.tabIndex = 0;
+        logoLink.focus();
+        btnBurger.classList.remove('header__burger_active');
+        headerNav.classList.remove('header__nav_active');
+      })
+
+    } else {
+      logoLink.tabIndex = 0;
     }
+
   });
+
 
 })
